d247eb6c2e39530a6d3b126459d2b3e458593430
Kukieu <stiekerosiem@gmail.com>
2020-06-23 20:26:59 +0200

    add training endpoint

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const accountRouter = require('./api/account');
+const trainingRouter = require('./api/training');
 const swaggerUi = require('swagger-ui-express');
 const specs = require('./api/docs/specs');
 
@@ -12,8 +13,9 @@ app.use(cors({ origin: 'http://192.168.1.28:1234', credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/account', accountRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/account', accountRouter);
+app.use('/training', trainingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.StatusCode || 500;
---
+++ app/training/table.js
@@ -0,0 +1,20 @@
+const { connectTo } = require('../../secrets/databaseConfiguration');
+
+class TrainingTable {
+    static training({ databasename, limit, offset }) {
+        const schoolPool = connectTo(databasename);
+        return new Promise((resolve, reject) => {
+            schoolPool.query(
+                'SELECT * FROM traininglist LIMIT $1 OFFSET $2',
+                [limit, offset],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve({ training: response.rows });
+                }
+            );
+        });
+    }
+}
+
+module.exports = TrainingTable;
