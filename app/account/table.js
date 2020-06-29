c52e348f7683504b4f5d543b6ee3435af6d4df1c
Kukieu <stiekerosiem@gmail.com>
2020-06-29 20:06:03 +0200

    add trainings endpoint

--- app/account/table.js
+++ app/account/table.js
@@ -46,6 +46,21 @@ class AccountTable {
             );
         });
     }
+
+    static trainings({ databasename, limit, offset }) {
+        const schoolPool = connectTo(databasename);
+        return new Promise((resolve, reject) => {
+            schoolPool.query(
+                'SELECT * FROM traininglist LIMIT $1 OFFSET $2',
+                [limit, offset],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve(response.rows);
+                }
+            );
+        });
+    }
 }
 
 module.exports = AccountTable;
