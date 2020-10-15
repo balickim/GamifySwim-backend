091aa96da0f8b2e3014fb80d9b6d82fa7c52d190
Kukieu <stiekerosiem@gmail.com>
2020-10-15 17:26:50 +0200

    samesite test

--- app/api/helper.js
+++ app/api/helper.js
@@ -33,8 +33,8 @@ const setSessionCookie = ({ sessionString, res }) => {
     res.cookie('sessionString', sessionString, {
         expire: Date.now() + 3600000,
         httpOnly: true,
-        sameSite: 'none',
-        secure: true // use with https
+        secure: true, // use with https
+        sameSite: 'none'
     });
 }
 
