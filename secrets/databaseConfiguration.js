c9f0a8ded0488534ef7e48f890dc4d66541df664
Kukieu <stiekerosiem@gmail.com>
2020-10-15 16:37:05 +0200

    move postgres db from localhost to oracle cloud

--- secrets/databaseConfiguration.js
+++ secrets/databaseConfiguration.js
@@ -2,19 +2,19 @@ const { Pool } = require('pg');
 
 const catalogPool = ({
     user: 'swimmer',
-    host: 'localhost',
+    host: '130.61.89.76',
     database: 'catalog',
-    password: 'swimmer',
-    port: 5433
+    password: 'jjU;jEQFQ>$p?{Aohs,>[L*wT',
+    port: 5432
 });
 
 const connectTo = databasename => {
     return new Pool({
         user: 'swimmer',
-        host: 'localhost',
+        host: '130.61.89.76',
         database: databasename,
-        password: 'swimmer',
-        port: 5433
+        password: 'jjU;jEQFQ>$p?{Aohs,>[L*wT',
+        port: 5432
     });
 }
 
